56d7f4c225ae170bd3c08642b3602de7cf823b38
Wence Cajucom <cajucom.w@gmail.com>
2019-07-19 11:00:39 -0700

    created view function for 'game.html'. Need to get the questions and answers to show up on page.

--- trivia/views.py
+++ trivia/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
+import requests
+import random
+from django.shortcuts import render
 
-# Create your views here.
+response = requests.get('https://opentdb.com/api.php?amount=10')
+data = response.json()
+user_answer = None
+
+correct_list = []
+# def welcome():
+
+#     print("Welcome to Our Group Project!")
+#     username = input("Please enter a Username: ")
+    
+# welcome()
+
+def game():
+    # while True:
+    for item in data['results']:            # get to the right list
+        question = item['question']
+        correct = item['correct_answer']    # reference keys for correct answers
+        choices = item['incorrect_answers'] # reverence keys for incorrect answers
+        print(question)
+
+        answers = []
+        for choice in choices:
+            answers.append(choice)      # this adds the wrong answers to the list "answers"
+        answers.append(correct)         # this adds the right answer to he list "answers"
+        random.shuffle(answers)         # this randomizes all the answers from the list "answers"
+        
+        print(answers)
+        
+        user_answer = input("Type your answer here: ")
+        if user_answer in correct:
+            correct_list.append(user_answer)
+            print("You got it!")
+        else:
+            print("Sorry. The answer is", correct + ".")
+        
+        print(correct_list)
+
+def scores():
+    correct_number = 0
+    correct_number = len(correct_list)
+    score = correct_number * 10
+    print("You scored", score, "%")
+
+# game()
+
+# scores()
+
+def gamepage(request):
+    context = {}
+    return render(request, 'game.html', context)
