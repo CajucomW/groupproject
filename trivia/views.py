e4c44f77e5014c48f9a10876729730ab70d415aa
Wence Cajucom <cajucom.w@gmail.com>
2020-01-20 22:30:11 -0800

    removed commented out code. reviewing existing code to continue work. quiz/trivia app currently functional

--- trivia/views.py
+++ trivia/views.py
@@ -12,13 +12,6 @@ from html.parser import HTMLParser
 
 correct_list = []
 
-# def gamepage(request):
-#     if request.method == 'GET':
-#         ask_question(request)
-#     else:
-#         check_answer(request)
-    
-#     return render(request, 'game.html', context)
 def gamepage(request):
     if request.method == 'GET':
         response = requests.get('https://opentdb.com/api.php?amount=1&type=multiple')
@@ -28,17 +21,14 @@ def gamepage(request):
             question = item['question']
             request.session['correct_answer'] = item['correct_answer']
             request.session['incorrect_answers'] = item['incorrect_answers']
+                # request.session allows data from two different dictionaries to persist through the requests
+                # and allow them to be combined into one list
 
             h = HTMLParser()
             question = h.unescape(question)
 
-            # context = {
-            # 'trivia': question,
-            # }
-
             print(question)
 
-            # for item in data['results']:
             correct = request.session['correct_answer']    # reference keys for correct answers
             choices = request.session['incorrect_answers'] # reverence keys for incorrect answers
             
@@ -62,10 +52,8 @@ def gamepage(request):
             'answer3': answers[3],
             'correct': correct,
         }
-        # return render(request, 'game.html', context)
 
     else:
-        # for item in data['results']:
         correct = request.session['correct_answer']    # reference keys for correct answers
         choices = request.session['incorrect_answers'] # reverence keys for incorrect answers
         
@@ -135,41 +123,6 @@ def gamepage(request):
 
     return render(request, 'game.html', context)
 
-
-
-# def gamepage(request):
-
-#     response = requests.get('https://opentdb.com/api.php?amount=1&type=multiple')
-#     data = response.json()
-    
-#     print("Viewed Gamepage")
-#     for item in data['results']:            # get to the right list
-#         question = item['question']
-#         correct = item['correct_answer']    # reference keys for correct answers
-#         choices = item['incorrect_answers'] # reverence keys for incorrect answers
-        
-#         answers = []        
-#         for choice in choices:
-#             answers.append(choice)      # this adds the wrong answers to the list "answers"
-#         answers.append(correct)         # this adds the right answer to he list "answers"
-#         random.shuffle(answers)         # this randomizes all the answers from the list "answers"
-#         correct_list.append(request.POST)
-
-#         # This code fixes the 'double' escape problem
-#         # that produces garbage text during ' and "
-#         h = HTMLParser()
-#         question = h.unescape(question)
-#         answers = h.unescape(answers)
-
-#         context = {
-#         'trivia': question,
-#         'answer0': answers[0],
-#         'answer1': answers[1],
-#         'answer2': answers[2],
-#         'answer3': answers[3],
-#         'correct': correct,
-#     }
-
 # ################### TODO PROBLEM LIST ###################
 # # correct/incorrect answers broke when random questions 
 # # was fixed.
@@ -179,54 +132,6 @@ def gamepage(request):
 # # The answer is wrong every time 'cause the set of 
 # # answers are different already.
 
-#         print(question)
-#         print(answers)
-#         print(correct)
-#         if 'a0' in request.POST:
-#             print("checked a0")
-#             answerzero = request.POST['a0']
-#             print("---", answerzero, "---")
-#             if answerzero == correct:
-#                 print("---Correct!---")
-#                 return redirect('correct/')
-#             else:
-#                 print("---Picked 0, wrong answer---")
-#                 return redirect('incorrect/')
-        
-#         if 'a1' in request.POST:
-#             print("checked a1")
-#             answerone = request.POST['a1']
-#             print("---", answerone, "---")
-#             if answerone == correct:
-#                 print("---Correct!---")
-#                 return redirect('correct/')
-#             else:
-#                 print("---Picked 1, wrong answer---")
-#                 return redirect('incorrect/')
-
-#         if 'a2' in request.POST:
-#             print("checked a2")
-#             answertwo = request.POST['a2']
-#             print("---", answertwo, "---")
-#             if answertwo == correct:
-#                 print("---Correct!---")
-#                 return redirect('correct/')
-#             else:
-#                 print("---Picked 2, wrong answer---")
-#                 return redirect('incorrect/')
-
-#         if 'a3' in request.POST:
-#             print("checked a3")
-#             answerthree = request.POST['a3']
-#             print("---", answerthree, "---")
-#             if answerthree == correct:
-#                 print("---Correct!---")
-#                 return redirect('correct/')
-#             else:
-#                 print("---Picked 3, wrong answer---")
-#                 return redirect('incorrect/')
-
-#     return render(request, 'game.html', context)
 
 def scores():
     correct_number = 0
