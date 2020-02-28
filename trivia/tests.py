4d41bdf59f0c5a7ee0dc9fd7a2a2735819123cf0
Wence Cajucom <cajucom.w@gmail.com>
2020-02-27 18:12:15 -0800

    edited testing code

--- trivia/tests.py
+++ trivia/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+class LinkCheckTestCase(TestCase):
+
+    def test_shows_login(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'Login Page')
+
+    def test_shows_game_page(self):
+        response = self.client.get('/game/')
+        self.assertContains(response, 'get started!')
